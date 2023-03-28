0ea1b921114afab7ae0b6b32b8641eeffe15963c
Sam Samskies <samsamskies@gmail.com>
2023-03-27 19:55:08 -0600

    update to gpt-4

--- utils/openai.ts
+++ utils/openai.ts
@@ -7,7 +7,7 @@ const openai = new OpenAIApi(configuration);
 
 export const createUserFortune = async (pubkey: string, content: string) => {
   const res = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
+    model: "gpt-4",
     messages: [{ role: "user", content }],
     user: pubkey,
   });
