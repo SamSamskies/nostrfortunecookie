b621008c3af2404f38f81ae3af72717da9646a7c
Sam Samskies <samsamskies@gmail.com>
2023-05-15 18:44:51 -0500

    add links in footer

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,6 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Image from "next/image";
+import React from "react";
 
 export default function Document() {
   return (
@@ -6,6 +8,26 @@ export default function Document() {
       <Head />
       <body>
         <Main />
+        <footer>
+          <a href="https://github.com/SamSamskies" target="_blank">
+            <Image
+              src="/github-mark.png"
+              width={32}
+              height={32}
+              alt="GitHub logo"
+            />
+          </a>
+          <p
+            id="nostr-zap-target"
+            data-npub="npub1f948ng6s3spk9wv990tuyh4dl0uujt6jq9uduanzt3yy653e6f6s77uvzg"
+          >
+            ⚡️
+          </p>
+        </footer>
+        <script
+          async
+          src="https://cdn.jsdelivr.net/npm/nostr-zap@0.1.6"
+        ></script>
         <NextScript />
       </body>
     </Html>
